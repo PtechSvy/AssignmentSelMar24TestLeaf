aedadf1afd83c0223d8f2a401fe60c874587287f
ptechsvy <ppodder59@outlook.com>
2024-03-27 19:07:29 +0530

    Collections home assignments

--- src/test/java/SeleniumSessions/HomeAssignment1/CollectionsAssign/MissingNumFromList.java
+++ src/test/java/SeleniumSessions/HomeAssignment1/CollectionsAssign/MissingNumFromList.java
@@ -1,36 +1,33 @@
 package SeleniumSessions.HomeAssignment1.CollectionsAssign;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class MissingNumFromList {
-//needs to be corrected
+
     public static void main(String[] args) {
 
-        List<Integer> missnValues = new ArrayList<>();
         int[] arr1 = {1, 2, 3, 4, 10, 6, 8};
-        for(int num:arr1)
-        {
-            missnValues.add(num);
+        Arrays.sort(arr1);
+
+        // Initialize a list to store missing numbers
+        List<Integer> missingNumbers = new ArrayList<>();
+
+        // Iterate through the array to find missing numbers
+        int expected = arr1[0]; // Start with the first element
+        for (int num : arr1) {
+            while (expected < num) {
+                missingNumbers.add(expected);
+                expected++;
+            }
+            expected++; // Move to the next expected number
         }
 
-        Collections.sort(missnValues);
-        int a,b;
-        List<Integer> missListVal = new ArrayList<>();
-       int val = missnValues.size();
-       for(int i = 0 ;i<val;i++)
-       {
-           {
-               a = missnValues.get(i)+1;
-               b = missnValues.get(i);
-               if(a!=b)
-               {
-                   missListVal.add(a);
-               }
-           }
-       }
-        System.out.println("Missing values in the list are" + " " + missListVal);
+        // Print the missing numbers
+        System.out.println("Missing numbers from the array: " + missingNumbers);
+
 
     }
 }
